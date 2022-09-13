ccaefe6fd4796802ce9ba7a122f9de58cb0e393b
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 19:09:57 +0200

    changes

--- install.sh
+++ install.sh
@@ -8,13 +8,14 @@ if [ "$1" = "gcloud" ]; then
     apt-get -y install python openssl unzip wget
 	curl https://raw.githubusercontent.com/MasterDevX/Termux-Java/master/installjava | bash
 	echo "Installing gcloud"
+	if [ ! -e " google-cloud-cli-401.0.0-linux-x86.tar.gz" ]; then
 	curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
 	tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
-	rm -f google-cloud-cli-401.0.0-linux-x86.tar
 	bash google-cloud-sdk/install.sh -q
+	fi
 elif [ "$1" = "appzardFiles" ]; then
 	echo "Installing appengine components"
-	bash google-cloud-sdk/bin/gcloud components install app-engine-java -y -q
+	bash google-cloud-sdk/bin/gcloud components install app-engine-java -q
 	echo "Downloading appzard Build Files"
 	curl "https://www.googleapis.com/drive/v3/files/1wobXDJaz9M2tYW87wd-SMe0O91xY72u-?alt=media&key=AIzaSyCuQUoy9CWSIx5LbLg3zCahsDgSMyV099w" > build.zip
 	unzip -q build.zip
