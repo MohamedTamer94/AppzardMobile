7a105a67f3e619858a051c99db9b6cdc3bd1bd8f
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 16:33:22 +0200

    changes

--- install.sh
+++ install.sh
@@ -7,4 +7,4 @@ apt-get -qq --force-yes -y install python openssl
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
 bash google-cloud-sdk/install.sh -q
-bash google-cloud-sdk/bin/gcloud init -q
+bash google-cloud-sdk/bin/gcloud install app-engine-java -y -q
