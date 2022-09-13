d24c7585328099be9c29a6eb9c1b6f3823c58758
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 18:30:53 +0200

    changes

--- install.sh
+++ install.sh
@@ -4,8 +4,8 @@ echo "Installing Appzard.."
 echo "$1"
 if [ "$1" = "gcloud" ]; then
 	echo "Installing Java"
-	pkg -y upgrade
-    pkg -y install python openssl unzip wget
+	apt-get -y upgrade
+    apt-get -y install python openssl unzip wget
 	curl https://raw.githubusercontent.com/MasterDevX/Termux-Java/master/installjava | bash
 	echo "Installing gcloud"
 	curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
