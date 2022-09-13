a543eba8284cbf0485565da1aee2982f2e1cf3c8
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 20:34:45 +0200

    changes

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 set -e
 cd build/war
 mkdir ~/tmp
-bash java -Dfile.encoding=UTF-8 -Djava.io.tempdir=~/tmp -classpath /data/data/com.termux/files/home/google-cloud-sdk/platform/google_appengine/google/appengine/tools/java/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --property=kickstart.user.dir=/data/data/com.termux/files/home/build/war --no_java_agent --promote_yaml --port=8888 --address=0.0.0.0 /data/data/com.termux/files/home/build/war/
+bash java -Dfile.encoding=UTF-8 -Djava.io.tmpdir=~/tmp -classpath /data/data/com.termux/files/home/google-cloud-sdk/platform/google_appengine/google/appengine/tools/java/lib/appengine-tools-api.jar com.google.appengine.tools.development.DevAppServerMain --property=kickstart.user.dir=/data/data/com.termux/files/home/build/war --no_java_agent --promote_yaml --port=8888 --address=0.0.0.0 /data/data/com.termux/files/home/build/war/
