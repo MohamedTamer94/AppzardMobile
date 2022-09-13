b5446d78ce96c9b15727551e96dfb118625558c0
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 15:20:58 +0200

    changes

---
+++ install.sh
@@ -0,0 +1,10 @@
+#!/data/data/com.termux/files/usr/bin/bash
+set -e
+echo "Installing Appzard.."
+echo "Installing gcloud"
+pkg upgrade
+pkg install python openssl -y
+curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
+tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
+./google-cloud-sdk/install.sh
+./google-cloud-sdk/bin/gcloud init
