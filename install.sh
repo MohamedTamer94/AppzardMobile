bbb5f4e92abecb13192f7058e55f29b986d0d66f
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 18:42:51 +0200

    changes

--- install.sh
+++ install.sh
@@ -11,8 +11,9 @@ if [ "$1" = "gcloud" ]; then
 	curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
 	tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
 	bash google-cloud-sdk/install.sh -q
-	bash google-cloud-sdk/bin/gcloud install app-engine-java -y -q
 elif [ "$1" = "appzardFiles" ]; then
+	echo "Installing appengine components"
+	bash google-cloud-sdk/bin/gcloud install app-engine-java -y -q
 	echo "Downloading appzard Build Files"
 	curl "https://www.googleapis.com/drive/v3/files/1wobXDJaz9M2tYW87wd-SMe0O91xY72u-?alt=media&key=AIzaSyCuQUoy9CWSIx5LbLg3zCahsDgSMyV099w" > build.zip
 	unzip -q build.zip
