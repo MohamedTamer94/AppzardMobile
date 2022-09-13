d2297f3a5e1745948277a1a22457d446fe9dcd8e
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 18:05:45 +0200

    changes

--- install.sh
+++ install.sh
@@ -1,10 +1,20 @@
 #!/data/data/com.termux/files/usr/bin/bash
 set -e
 echo "Installing Appzard.."
-echo "Installing gcloud"
-pkg upgrade -y
-apt-get -qq --force-yes -y install python openssl
-curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
-tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
-bash google-cloud-sdk/install.sh -q
-bash google-cloud-sdk/bin/gcloud install app-engine-java -y -q
+if [ "$1" == "gcloud" ]; then
+	echo "Installing Java"
+	curl https://raw.githubusercontent.com/MasterDevX/Termux-Java/master/installjava | bash
+	echo "Installing gcloud"
+	pkg upgrade -y
+	apt-get -qq --force-yes -y install python openssl unzip
+	curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
+	tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
+	bash google-cloud-sdk/install.sh -q
+	bash google-cloud-sdk/bin/gcloud install app-engine-java -y -q
+elif [ "$2" == "appzardFiles" ]; then
+	echo "Downloading appzard Build Files"
+	curl https://www.googleapis.com/drive/v3/files/1wobXDJaz9M2tYW87wd-SMe0O91xY72u-?alt=media&key=AIzaSyCuQUoy9CWSIx5LbLg3zCahsDgSMyV099w > build.zip
+	unzip --output build build.zip
+	echo "Done!"
+	bash google-cloud-sdk/bin/java_dev_appserver.sh
+fi
