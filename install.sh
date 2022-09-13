f56eed8794b5ed76fb4217866eff1c9826a3bcd7
MohamedTamer94 <mohamed_tamer_ahmed@yahoo.com>
2022-09-13 15:49:22 +0200

    changes

--- install.sh
+++ install.sh
@@ -6,5 +6,5 @@ pkg upgrade
 pkg install python openssl -y
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-401.0.0-linux-x86_64.tar.gz
 tar -xf google-cloud-cli-401.0.0-linux-x86.tar.gz
-./google-cloud-sdk/install.sh -y
+./google-cloud-sdk/install.sh --help
 ./google-cloud-sdk/bin/gcloud init
